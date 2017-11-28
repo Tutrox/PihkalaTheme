82ac87a395874e87c2a073233f55e3b53276cfbd
Klooven <Klooven@users.noreply.github.com>
2017-11-28 16:57:41 +0200

    Added no title template. Closes #2

---
+++ no-title.php
@@ -0,0 +1,11 @@
+<?php /*Template Name: No title*/
+get_header();
+if (have_posts()) :
+	while (have_posts()) : the_post(); ?>
+	<?php the_content(); ?>
+	<?php endwhile;
+else :
+	errorText();
+endif;
+get_footer();
+?>
