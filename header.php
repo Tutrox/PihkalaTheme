d283e357e763777090b442ae978964449e2e5acc
Klooven <Klooven@users.noreply.github.com>
2020-09-15 18:39:19 +0000

    Changed separator in title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title><?php bloginfo('name'); ?> ~ <?php the_title(); ?></title>
+  <title><?php bloginfo('name'); ?> | <?php the_title(); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
