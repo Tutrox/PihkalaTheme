0ea535c9e2936b903bf65b4b471652119c60bbc6
Klooven <Klooven@users.noreply.github.com>
2020-09-15 18:34:21 +0000

    Prettier has cleaned the PHP

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo ('charset'); ?>" />
+  <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title><?php bloginfo ('name'); ?> ~ <?php the_title(); ?></title>
+  <title><?php bloginfo('name'); ?> ~ <?php the_title(); ?></title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div class="pihkala-brand" <?php if (has_header_image()) { $bg = esc_url(get_header_image()); echo "style=\"background-image: url($bg);\""; } ?>>
+  <div class="pihkala-brand" <?php if (has_header_image()) {
+    $bg = esc_url(get_header_image());
+    echo "style=\"background-image: url($bg);\"";
+  } ?>>
     <div class="container-fluid">
       <a href="<?php echo esc_url(home_url('/')); ?>">
         <h1 class="display-3"><?php bloginfo('name'); ?></h1>
@@ -20,13 +23,11 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="nav">
-      <?php
-      wp_nav_menu( array(
+      <?php wp_nav_menu([
         'menu' => 'navbar-menu',
         'menu_class' => 'nav-menu',
-        'container' => null
-      ) );
-      ?>
+        'container' => null,
+      ]); ?>
     </div>
   </nav>
   <div class="container-fluid">
--- no-title.php
+++ no-title.php
@@ -1,11 +1,13 @@
 <?php /*Template Name: No title*/
 get_header();
-if (have_posts()) :
-	while (have_posts()) : the_post(); ?>
+if (have_posts()):
+  while (have_posts()):
+    the_post(); ?>
 	<?php the_content(); ?>
-	<?php endwhile;
-else :
-	errorText();
+	<?php
+  endwhile;
+else:
+  errorText();
 endif;
 get_footer();
 ?>
